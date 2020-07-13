7d57aadc284e5c85ef8256898deff9d4fa63813c
Mohammad Bakhshalipour <mbakhsha@h0.run-champsim.prescriptivemem.narwhal.pdl.cmu.edu>
2020-07-12 21:36:04 -0400

    Some amends in run_champsim script

--- run_champsim.sh
+++ run_champsim.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-if [ "$#" -lt 4 ]; then
+TRACE_DIR=/proj/PrescriptiveMem/benchmarks/champsim-traces
+
+if [ "$#" -lt 5 ]; then
     echo "Illegal number of parameters"
-    echo "Usage: ./run_champsim.sh [BINARY] [N_WARM] [N_SIM] [TRACE] [OPTION]"
+    echo "TRACE_DIR: $TRACE_DIR"
+    echo "Usage: ./run_champsim.sh [BINARY] [N_WARM]M [N_SIM]M [TRACE] [EXPERIMENT_NAME] [OPTION]"
     exit 1
 fi
 
-TRACE_DIR=$PWD/dpc3_traces
 BINARY=${1}
+BINARY=${BINARY#bin/}   # Remove 'bin/' from the beginning if exists
+
 N_WARM=${2}
 N_SIM=${3}
+
+# $TRACE should be the absolute path of the final trace file
 TRACE=${4}
-OPTION=${5}
+TRACE_NAME=${TRACE##*/}
+TRACE_NAME=${TRACE_NAME%.champsimtrace.xz}
+
+EXPERIMENT_NAME=${5}
+OPTION=${6}
 
 # Sanity check
-if [ -z $TRACE_DIR ] || [ ! -d "$TRACE_DIR" ] ; then
-    echo "[ERROR] Cannot find a trace directory: $TRACE_DIR"
-    exit 1
-fi
-
 if [ ! -f "bin/$BINARY" ] ; then
     echo "[ERROR] Cannot find a ChampSim binary: bin/$BINARY"
     exit 1
@@ -36,10 +41,18 @@ if ! [[ $N_SIM =~ $re ]] || [ -z $N_SIM ] ; then
     exit 1
 fi
 
-if [ ! -f "$TRACE_DIR/$TRACE" ] ; then
-    echo "[ERROR] Cannot find a trace file: $TRACE_DIR/$TRACE"
+if [ ! -f "$TRACE" ] ; then
+    echo "[ERROR] Cannot find a trace file: $TRACE"
     exit 1
 fi
 
-mkdir -p results_${N_SIM}M
-(./bin/${BINARY} -warmup_instructions ${N_WARM}000000 -simulation_instructions ${N_SIM}000000 ${OPTION} -traces ${TRACE_DIR}/${TRACE}) &> results_${N_SIM}M/${TRACE}-${BINARY}${OPTION}.txt
+RES_DIR="results/results_${EXPERIMENT_NAME}"
+RES_FILE_NAME="${TRACE_NAME}-${BINARY}${OPTION}.txt"
+
+if [[ -f ${RES_DIR}/${RES_FILE_NAME} ]]; then
+    read -p "Overwriting? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+fi
+
+mkdir -p $RES_DIR
+(./bin/${BINARY} -warmup_instructions ${N_WARM}000000 -simulation_instructions ${N_SIM}000000 ${OPTION} -traces ${TRACE}) &> ${RES_DIR}/${TRACE_NAME}-${BINARY}${OPTION}.txt
+
